22a8b36f1197b32847c92577a0d0c6a572b7fc61
Ceejayoor <154600443+Ceejayoor@users.noreply.github.com>
2024-09-14 04:19:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,52 @@
 const cardArray = [
     {
         name: "milkshake",
-        img: "/images for games/milkshake.png",
+        img: "./images for games/milkshake.png",
     },
     {
         name: "ice-cream",
-        img: "/images for games/ice-cream.png",
+        img: "./images for games/ice-cream.png",
     },
     {
         name: "pizza",
-        img: "/images for games/pizza.png",
+        img: "./images for games/pizza.png",
     },
     {
         name: "fries",
-        img: "/images for games/fries.png",
+        img: "./images for games/fries.png",
     },
     {
         name: "hotdog",
-        img: "/images for games/hotdog.png",
+        img: "./images for games/hotdog.png",
     },
     {
         name: "cheeseburger",
-        img: "/images for games/cheeseburger.png",
+        img: "./images for games/cheeseburger.png",
     },
    
     {
         name: "milkshake",
-        img: "/images for games/milkshake.png",
+        img: "./images for games/milkshake.png",
     },
     {
         name: "ice-cream",
-        img: "/images for games/ice-cream.png",
+        img: "./images for games/ice-cream.png",
     },
     {
         name: "pizza",
-        img: "/images for games/pizza.png",
+        img: "./images for games/pizza.png",
     },
     {
         name: "fries",
-        img: "/images for games/fries.png",
+        img: "./images for games/fries.png",
     },
     {
         name: "hotdog",
-        img: "/images for games/hotdog.png",
+        img: "./images for games/hotdog.png",
     },
     {
         name: "cheeseburger",
-        img: "/images for games/cheeseburger.png",
+        img: "./images for games/cheeseburger.png",
     }
     ];
 
@@ -57,7 +57,7 @@ cardArray.sort(()=> 0.5 - Math.random());
 function createBoard(){
     for(i=0; i<cardArray.length; i++){
     let card = document.createElement("img");
-    card.setAttribute("src", "/images for games/blank.png");
+    card.setAttribute("src", "./images for games/blank.png");
     card.setAttribute("data-id", i);
     card.addEventListener("click", flipCard); 
     theGrid.append(card);
@@ -87,8 +87,8 @@ const cards = document.querySelectorAll("img");
 
     if(cardChosen[0]===cardChosen[1]){
         alert("you found a match");
-        cards[cardChosenIds[0]].setAttribute("src", "/images for games/white.png");
-        cards[cardChosenIds[1]].setAttribute("src", "/images for games/white.png");
+        cards[cardChosenIds[0]].setAttribute("src", "./images for games/white.png");
+        cards[cardChosenIds[1]].setAttribute("src", "./images for games/white.png");
         cards[cardChosenIds[0]].removeEventListener("click", flipCard);
         cards[cardChosenIds[1]].removeEventListener("click",  flipCard);
         cardsWon.push(cardChosen);
@@ -97,8 +97,8 @@ const cards = document.querySelectorAll("img");
     }
 
     else{
-        cards[cardChosenIds[0]].setAttribute("src", "/images for games/blank.png");
-        cards[cardChosenIds[1]].setAttribute("src", "/images for games/blank.png");
+        cards[cardChosenIds[0]].setAttribute("src", "./images for games/blank.png");
+        cards[cardChosenIds[1]].setAttribute("src", "./images for games/blank.png");
     }
     cardChosen = [];
     cardChosenIds = [];
